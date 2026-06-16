0658907940d348a665ea2fe9ba107a93bbf9f915
Andrew McInnes <acmcinnes@outlook.com>
2026-06-16 13:00:06 +1000

    Allow Neto Application

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,10 +1,18 @@
-import { createAuthClient } from "better-auth/react"
-import { genericOAuthClient } from "better-auth/client/plugins"
+import { createAuthClient } from "better-auth/react";
+import { genericOAuthClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
-    callbackURL: `/account`,
-    plugins: [
-        genericOAuthClient() 
-    ]
-})
+  baseURL: process.env.BETTER_AUTH_URL,
+  callbackURL: `/account`,
+  plugins: [genericOAuthClient()],
+  fetchOptions: {
+    onError: async (context) => {
+      const { response } = context;
+      if (response.status === 429) {
+        const retryAfter = response.headers.get("X-Retry-After");
+        console.log(`Rate limit exceeded. Retry after ${retryAfter} seconds`);
+        throw new Error('Request blocked, try again later...')
+      }
+    },
+  },
+});
